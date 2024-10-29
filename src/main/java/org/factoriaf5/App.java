84e222a49b48a7af8ec7c733f1b75e1203f3dba0
Seal Kevin Boy Abad <sealkevinboyabad@gmail.com>
2024-10-29 02:33:28 +0100

    añadidas constantes y clase principal

--- src/main/java/org/factoriaf5/App.java
+++ src/main/java/org/factoriaf5/App.java
@@ -1,13 +1,12 @@
-package org.factoriaf5;
+public class IMCCalculator {
+    private static final double BAJO_PESO_MAX = 18.5;
+    private static final double PESO_NORMAL_MAX = 24.9;
+    private static final double SOBREPESO_MAX = 29.9;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    private final double peso;
+    private final double estatura;
+
+    public IMCCalculator(double peso, double estatura) {
+        this.peso = peso;
+        this.estatura = estatura; 
     }
-}
